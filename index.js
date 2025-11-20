ea8b1631149f3a01cf1bd8204bf11a5a7c3c0b86
zjk233 <zjk233@zjk233.top>
2025-11-20 19:07:23 +0800

    Initial commit2

--- index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request) {
-    const html = await fetch(new URL("./index.html", import.meta.url)).then(r => r.text());
-    return new Response(html, {
-      headers: { "Content-Type": "text/html" }
-    });
-  }
-}
